57f73315f86361bd7aa2e5699d2da45176fd0613
Alberto Sottile <alby128@gmail.com>
2019-02-05 09:26:11 +0100

    TLS: fix server certificate loading

--- syncplayServer.py
+++ syncplayServer.py
@@ -19,10 +19,16 @@ from twisted.internet.endpoints import SSL4ServerEndpoint
 
 from syncplay.server import SyncFactory, ConfigurationGetter
 
-with open('server.pem') as f:
-    certData = f.read()
+tlsCertPath = 'cert'
+privkey = open(tlsCertPath+'/privkey.pem', 'rt').read()
+certif = open(tlsCertPath+'/cert.pem', 'rt').read()
+chain = open(tlsCertPath+'/chain.pem', 'rt').read()
 
-certificate = ssl.PrivateCertificate.loadPEM(certData).options()
+privkeypyssl = crypto.load_privatekey(crypto.FILETYPE_PEM, privkey)
+certifpyssl = crypto.load_certificate(crypto.FILETYPE_PEM, certif)
+chainpyssl = [crypto.load_certificate(crypto.FILETYPE_PEM, chain)]
+
+contextFactory = ssl.CertificateOptions(privateKey=privkeypyssl, certificate=certifpyssl, extraCertChain=chainpyssl)
 
 if __name__ == '__main__':
     argsGetter = ConfigurationGetter()
@@ -39,8 +45,8 @@ if __name__ == '__main__':
         args.max_username_length,
         args.stats_db_file
     )
-    endpoint4 = SSL4ServerEndpoint(reactor, int(args.port), certificate, interface='0.0.0.0')
+    endpoint4 = SSL4ServerEndpoint(reactor, int(args.port), contextFactory, interface='0.0.0.0')
     endpoint4.listen(factory)
-    endpoint6 = SSL4ServerEndpoint(reactor, int(args.port), certificate, interface='::')
+    endpoint6 = SSL4ServerEndpoint(reactor, int(args.port), contextFactory, interface='::')
     endpoint6.listen(factory)
     reactor.run()
