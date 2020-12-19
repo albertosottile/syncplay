472cc3ac7a5ae8745bf2b19dcc0b0cd3217fc2c8
Alberto Sottile <alby128@gmail.com>
2020-12-19 15:57:24 +0100

    Patch pyside2 linker

--- travis/pyside2_linker.py
+++ travis/pyside2_linker.py
@@ -17,7 +17,8 @@ for fr in frameworks:
     fr_path = os.path.join(QT_LIB_PATH, fr)
     fr_name = fr.split('.framework')[0]
     os.chdir(fr_path)
-    make_symlink(f'Versions/Current/{fr_name}', fr_name)
-    os.chdir(os.path.join(fr_path, 'Versions'))
-    make_symlink('5', 'Current')
+    if 'Versions'  in os.listdir('.'):
+        make_symlink(f'Versions/Current/{fr_name}', fr_name)
+        os.chdir(os.path.join(fr_path, 'Versions'))
+        make_symlink('5', 'Current')
     os.chdir(QT_LIB_PATH)
